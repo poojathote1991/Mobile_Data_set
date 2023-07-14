a512b071199e743f21fa018e1eaa482a5c4120cc
poojathote1991 <poojaghode6@gmail.com>
2023-07-14 15:18:34 +0530

    config file update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-PORT_NUMBER=5003
+PORT_NUMBER=8080
 
 MONGODB_PORT_NUMBER=27017
 
-MODEL_FILE_PATH=r'static\model_file\mobileinfo_linear_regession_model.pkl'
+MODEL_FILE_PATH=os.path.join(r'static','model_file','mobileinfo_linear_regession_model.pkl')
